ce3e79b2175398485986dab7f0bd6d9db38bcfaa
Krzysztof <118080936+hunczak@users.noreply.github.com>
2026-04-12 22:51:23 +0100

    new

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ const App = () => (
       <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </HashRouter>
